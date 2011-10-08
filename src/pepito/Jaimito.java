473bbada579fb855f49dfedf0280517ff9c1fd27
Carlos Gochez <cgochez@lawgical.com>
2011-10-07 19:20:12 -0600

    Agregue la clase jaimito y le puse hola mundo a pepito

---
+++ src/pepito/Jaimito.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepito;
+
+/**
+ *
+ * @author CarlosArmando
+ */
+public class Jaimito {
+    private String nombre;
+    
+}
