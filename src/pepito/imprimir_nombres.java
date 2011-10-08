a7fb3ecf049d3703aeca4ccd6b30fa576116cae6
unknown <Sarahi@.(none)>
2011-10-07 20:20:20 -0600

    se agrego clase intento2

---
+++ src/pepito/imprimir_nombres.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pepito;
+
+/**
+ *
+ * @author Sarahi
+ */
+public class imprimir_nombres {
+    public static void main(String args[]){
+        System.out.println("Sarahi Lopez");
+        System.out.println("Hector Aguilar");
+    }
+}
