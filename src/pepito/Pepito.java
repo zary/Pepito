85ec1da50f8c0540a7fa72c7cc14cd8c5635087f
Carlos Gochez <cgochez@lawgical.com>
2011-10-07 19:31:12 -0600

    la cosa que faltaba

--- src/pepito/Pepito.java
+++ src/pepito/Pepito.java
@@ -15,6 +15,6 @@ public class Pepito {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo Pepito");
     }
 }
